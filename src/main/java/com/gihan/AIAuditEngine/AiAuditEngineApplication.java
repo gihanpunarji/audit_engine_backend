182c7abba8eae7febe24e6049a8c82e91cd53c7f
Gihan <gihanpunarji@gmail.com>
2026-07-15 19:31:29 +0530

    Initial Project

---
+++ src/main/java/com/gihan/AIAuditEngine/AiAuditEngineApplication.java
@@ -0,0 +1,14 @@
+package com.gihan.AIAuditEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAuditEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAuditEngineApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/com/gihan/AIAuditEngine/AiAuditEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gihan.AIAuditEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAuditEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
