a91072f7b936bf94956113790ec793b132e65ea7
Gihan <gihanpunarji@gmail.com>
2026-07-18 16:17:41 +0530

    modification: removve unused imports

--- src/main/java/com/gihan/AIAuditEngine/controller/UserController.java
+++ src/main/java/com/gihan/AIAuditEngine/controller/UserController.java
@@ -1,11 +1,5 @@
 package com.gihan.AIAuditEngine.controller;
 
-import com.gihan.AIAuditEngine.dto.UserRequestDTO;
-import com.gihan.AIAuditEngine.dto.UserResponseDTO;
-import com.gihan.AIAuditEngine.entity.User;
-import com.gihan.AIAuditEngine.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
